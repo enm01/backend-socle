10fca994cb9c515f0995b1ab17ecd93c3a18b3c4
imane <imaneziani2001@gmail.com>
2024-02-02 10:59:15 +0100

    code

---
+++ src/main/java/com/imane/socleappv1/SocleAppV1Application.java
@@ -0,0 +1,13 @@
+package com.imane.socleappv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocleAppV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocleAppV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/imane/socleappv1/SocleAppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imane.socleappv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocleAppV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
